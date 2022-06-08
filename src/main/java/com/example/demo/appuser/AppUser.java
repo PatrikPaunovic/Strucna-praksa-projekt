3078157df9e9c07fb9eb0e77ae10186815d12d5d
PatrikPaunovic <77941859+PatrikPaunovic@users.noreply.github.com>
2022-06-08 11:10:43 +0200

    Update AppUser.java

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -67,7 +67,7 @@ public class AppUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return email;
+        return username;
     }
 
     public String getFirstname(){
