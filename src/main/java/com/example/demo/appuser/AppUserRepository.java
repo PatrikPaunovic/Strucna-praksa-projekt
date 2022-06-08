9f297a0c7818c31efb98ed8838c93a5f1334980c
PatrikPaunovic <77941859+PatrikPaunovic@users.noreply.github.com>
2022-06-08 11:30:00 +0200

    Update AppUserRepository.java

--- src/main/java/com/example/demo/appuser/AppUserRepository.java
+++ src/main/java/com/example/demo/appuser/AppUserRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 @Repository
 @Transactional(readOnly = true)
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-
+    
+    Optional<AppUser> findByUsername(String username);
     Optional<AppUser> findByEmail(String email);
 }
